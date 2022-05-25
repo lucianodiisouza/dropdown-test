4a0c0f2098426e957430b6585ae48bc5b9eda9cb
Luciano dii Souza <lucianodiisouza@hotmail.com>
2022-05-25 17:28:55 -0300

    feat: passing elms to content

--- src/components/DropDown/components/Content/index.tsx
+++ src/components/DropDown/components/Content/index.tsx
@@ -1,24 +1,44 @@
 import DropDownMenuItem from "../Item";
-
-const items = [
-  {
-    label: "Save",
-  },
-  {
-    label: "Share",
-  },
-  {
-    label: "Delete",
-  },
-];
+import { Container } from "./styles";
 
 const DropDownContent = () => {
+  const deleteAction = () => {
+    console.log("do something from button");
+  };
+
+  const shareAction = () => {
+    console.log("do some action");
+  };
+
+  const items = [
+    {
+      id: 1,
+      element: <p>Rename</p>,
+    },
+    {
+      id: 2,
+      element: <button onClick={deleteAction}>Delete</button>,
+    },
+    {
+      id: 3,
+      element: <button onClick={shareAction}>Share</button>,
+    },
+    {
+      id: 4,
+      element: (
+        <a href="https://www.google.com" target="_blank">
+          Google
+        </a>
+      ),
+    },
+  ];
+
   return (
-    <>
-      {items.map(({ label }) => (
-        <DropDownMenuItem key={label}>{label}</DropDownMenuItem>
+    <Container>
+      {items.map(({ element, id }) => (
+        <DropDownMenuItem key={id}>{element}</DropDownMenuItem>
       ))}
-    </>
+    </Container>
   );
 };
 
