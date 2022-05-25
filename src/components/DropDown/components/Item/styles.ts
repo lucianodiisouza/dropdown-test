d6a1cd8cb977e5d4386c9fdd7d6187d81f3440f3
Luciano dii Souza <lucianodiisouza@hotmail.com>
2022-05-25 17:32:50 -0300

    feat: formatting a and button

--- src/components/DropDown/components/Item/styles.ts
+++ src/components/DropDown/components/Item/styles.ts
@@ -8,6 +8,22 @@ export const Container = styled.div`
   padding: 15px 10px;
   color: ${colors.text.white200};
 
+  button {
+    width: 100%;
+    height: 100%;
+    border: none;
+    color: ${colors.text.white200};
+    background: transparent;
+    text-align: left;
+  }
+
+  a {
+    display: flex;
+    flex: 1;
+    text-decoration: none;
+    color: ${colors.text.white200};
+  }
+
   &:hover {
     background-color: ${colors.general.blue400};
     transition: ease-in-out 0.2s;
